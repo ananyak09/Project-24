1c40a5bc4a775658299394b2ce37f710baa4fbe9
Ananya Kedia <69528295+ananyak09@users.noreply.github.com>
2020-09-02 21:05:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+var paper1 , ground1 , side1 , side2, bottom;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	paper1 = new Paper(50 , 640 );
+    ground = new Ground(400,660,800,20);
+	
+    
+	side1 = Bodies.rectangle(260,610,20,100);
+	World.add(world,side1);
+	side2 = Bodies.rectangle(460,610,20,100);
+	World.add(world,side2);
+	bottom = Bodies.rectangle(360,650,200,20);
+	World.add(world,bottom);
+	
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  Engine.update(engine);
+  paper1.display();
+  ground.display();
+  rect(260,610,20,100);
+  rect(460,610,20,100);
+  rect(360,650,200,20);
+ 
+}
+
+function keyPressed( ) {
+	if( keyCode === UP_ARROW){
+		Matter.Body.applyForce(paper1.body ,paper1.body.position , {x:50 ,y:-50});
+	}
+}
+
+
+
